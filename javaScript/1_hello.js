8b3741dea683939fd860018035e626817c2a3ad5
Vishal Kumar <139346808+VISHAL662004@users.noreply.github.com>
2025-03-23 09:25:32 +0530

    js

---
+++ javaScript/1_hello.js
@@ -0,0 +1,25 @@
+console.log("Hello JS!");
+
+fullName = "Tony Stakr";
+
+console.log(fullName);
+
+x = null;
+console.log(null);
+
+let z = 10;
+console.log(typeof z);
+
+// Object
+
+const student = {
+    fullNmae : "Vishal Kumar",
+    age : 20,
+    CGPA : 6.2,
+    isPass : true,
+};
+
+console.log(student);
+console.log(student["fullNmae"]);
+console.log(student.fullNmae);
+console.log(typeof student);
